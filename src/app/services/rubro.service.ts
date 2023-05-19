55779a4b664f45ead9aa6c52171fa09c39a7084f
mgMarquez <mgmarquez@gmail.com>
2023-05-19 01:36:22 -0300

    rubro servicio

--- src/app/services/rubro.service.ts
+++ src/app/services/rubro.service.ts
@@ -5,14 +5,30 @@ import { Rubro } from '../interfaces/rubro';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RubroService {
-  url: string = environment.apiUrl + '/rubro';
+  url: string = environment.apiUrl + '/rubros';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllRubros(): Observable<Rubro[]> {
     return this.http.get<Rubro[]>(this.url);
+  }
+
+  getRubro(id: number): Observable<Rubro> {
+    return this.http.get<Rubro>(this.url + `/${id}`);
+  }
+
+  createRubro(rubro: Rubro): Observable<Rubro> {
+    return this.http.post<Rubro>(this.url, rubro);
+  }
+
+  updateRubro(rubro: Rubro): Observable<Rubro> {
+    return this.http.put<Rubro>(this.url + `/${rubro.id}`, rubro);
+  }
+
+  deleteRubro(id: number): Observable<any>{
+    return this.http.delete(this.url + `/${id}`);
   }
 }
