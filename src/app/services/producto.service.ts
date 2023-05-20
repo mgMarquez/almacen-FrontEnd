2fc2b73ddfe75650b67ffdad00474e332a31a6de
mgMarquez <mgmarquez@gmail.com>
2023-05-19 23:30:17 -0300

    servicio producto

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -8,11 +8,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductoService {
-  url:string = environment.apiUrl + '/producto';
+  url:string = environment.apiUrl + '/productos';
 
   constructor(private http:HttpClient) { }
 
+  createProducto(producto: Producto): Observable<Producto> {
+    return this.http.post<Producto>(this.url, producto);
+  }
+
   getAllProductos(): Observable<Producto[]> {
     return this.http.get<Producto[]>(this.url);
+  }
+
+  getProducto(id: number): Observable<Producto> {
+    return this.http.get<Producto>(this.url + `/${id}`);
+  }
+
+  updateProducto(producto: Producto): Observable<Producto> {
+    return this.http.put<Producto>(this.url + `/${producto.id}`, producto);
+  }
+
+  deleteProducto(id: number): Observable<any> {
+    return this.http.delete(this.url + `/${id}`);
   }
 }
